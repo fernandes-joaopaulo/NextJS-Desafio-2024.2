919bbf4906df286224c03ccf1be9f5b637e74ad9
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2024-08-05 17:41:18 -0300

    Adiciona cores da paleta no Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens:{
+        'md': '768px',
+        'lg': '1280px',
+        'xl': '1920px',
+        '2xl': '2560px',
+        '3xl': '3440px',
+      },
+      colors:{
+        'amarelo': '#FDB833',
+        'azul': '#5A97D6',
+        'verde': '#49B637',
+        'vermelho': '#CB3636',
+      },
     },
   },
   plugins: [],
